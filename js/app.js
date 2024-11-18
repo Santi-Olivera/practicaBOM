30b23e92f4d2184fe13f8dccec1872e08c247973
Santiago Olivera <santiagoolivera1998@gmail.com>
2024-11-18 01:57:20 -0300

    Agregamos un ejemplo de setInterval

--- js/app.js
+++ js/app.js
@@ -10,7 +10,22 @@ function saludarPersona (nombre){
 }
 
 
+function saludarMundo(){
+    document.write(`<p>Hola mundo 🌍</p>`)
+    if (contador === 10) {
+        //Detener el setInterval
+        clearInterval(idInterval)
+    }
+    contador++; // contador= contador + 1
+}
+
+
 
 //setTimeout(saludar, 3000)
 setTimeout(()=> document.write(`<p>Hola 😁 </p>`), 4000)
 setTimeout(()=> saludarPersona(`Tomas`), 2000)
+
+
+//setinterval: ejecuta una funcion cada cierto tiempo en milisengundos
+let contador=1;
+const idInterval= setInterval(saludarMundo, 2000)
